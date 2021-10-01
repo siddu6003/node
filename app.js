a60142863658cbd8ff9d55df59a03275e15c95f6
siddu6003 <51458421+siddu6003@users.noreply.github.com>
2021-10-01 17:00:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const exp=require('express')
+const path = require('path');
+const router = exp.Router();
 app=exp()
 const port = process.env.port || 3000
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-})
 
 app.listen(port,function(req,res){
   console.log("listen")
 })
+
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
