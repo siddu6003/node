11bcac39e3a2ced979cd7fc5fde00f70af8fc3cd
siddu6003 <51458421+siddu6003@users.noreply.github.com>
2021-10-01 16:49:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const exp=require('express')
 app=exp()
 const port = process.env.port || 3000
 app.get('/', function(req, res){
-  res.send("hello")
+  res.render('index.html')
 })
 
 app.listen(port,function(req,res){
